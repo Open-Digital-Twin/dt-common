e6c54f13b68151a9ecfd31df3c01975c483d3357
Francisco Knebel <franciscopaivaknebel@gmail.com>
2020-06-28 02:20:31 -0300

    Get sources by element.

--- db/mod.rs
+++ db/mod.rs
@@ -16,6 +16,8 @@ use actix_web::{web};
 // Requires mod models
 use crate::common::models::app::{CurrentSession};
 
+use crate::common::models::twin::{Source};
+
 pub fn init_db_session() -> Arc<CurrentSession> {
   let db_address = env::var("DB_ADDRESS").unwrap();
 
@@ -65,4 +67,34 @@ pub fn get_by_id<T: TryFromRow>(session: web::Data<Arc<CurrentSession>>, item_id
     return Err(("No item found.".to_string(), 404));
   }
   return Ok(T::try_from_row(rows[0].clone()).unwrap());
+}
+
+pub fn get_element_sources(session: web::Data<Arc<CurrentSession>>, element_id: String) -> Result<Vec<Source>, (String, usize)> {
+  let id: Uuid;
+
+  match Uuid::parse_str(&element_id) {
+    Ok(_id) => { id = _id },
+    Err(_error) => {
+      match to_uuid(&element_id) {
+        Ok(_id) => { id = _id },
+        Err(_) => { return Err((format!("Invalid input."), 400)); }
+      }
+    }
+  }
+
+  let r = session.query(format!("SELECT * FROM source WHERE element = {}", id));
+
+  let rows = r.expect("Get sources by element")
+    .get_body().unwrap()
+    .into_rows().unwrap();
+
+  if rows.is_empty() {
+    return Ok(vec![]);
+  }
+
+  let mut sources: Vec<Source> = Vec::new();
+  for row in rows {
+    sources.push(Source::try_from_row(row).unwrap());
+  }
+  Ok(sources)
 }
